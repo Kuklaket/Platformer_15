1b9837856882e84ed0dd3fef4d373ef6117fcc61
Kuklaket <smakova.2001.ru@gmail.com>
2025-09-06 19:18:55 +0300

    0.2

--- Assets/Scripts/HashForTags.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class HashForTags : MonoBehaviour
-{
-    public static string Point = "Point";
-    public static string Run = "IsRun";
-    public static string Jump = "IsJump";
-}
